dfd436adec0bbbecf9395c4a7e8c4994813bb7ac
Jain-nikhilkumar <nikhilkumar1514@gmail.com>
2025-01-19 15:15:32 +0530

    1.0.3

--- client/client/src/App.js
+++ client/client/src/App.js
@@ -1,14 +1,39 @@
-// Purpose: Main component of the application. It is the parent component of the CollaborativeEditor component.
-import './App.css';
-import CollaborativeEditor from './CollaborativeEditor';
 
+// export default App;
+import React, { useState, useEffect } from "react";
+import { Editor } from "@monaco-editor/react";
+import io from "socket.io-client";
 
-  
+const socket = io("http://localhost:3000");
+
 function App() {
+  const [code, setCode] = useState("// Start coding here");
+
+  useEffect(() => {
+    // Listen for updates from other users
+    socket.on("codeChange", (newCode) => {
+      setCode(newCode);
+    });
+
+    // Clean up the socket listener on component unmount
+    return () => socket.off("codeChange");
+  }, []);
+
+  const handleEditorChange = (value) => {
+    setCode(value);
+    // Emit code changes to the server
+    socket.emit("codeChange", value);
+  };
+
   return (
-    <div className="App">
-      <h1>Real-Time Collaborative Coding Platform</h1>
-      <CollaborativeEditor />
+    <div style={{ height: "100vh" }}>
+      <Editor
+        height="90vh"
+        defaultLanguage="javascript"
+        value={code}
+        theme="vs-dark"
+        onChange={handleEditorChange}
+      />
     </div>
   );
 }
